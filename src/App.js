c1c92b3a7dd09b2a8893a726978cfd482fdacc98
Bharath Takkallapelly <tbharat2008@gmail.com>
2019-01-25 17:05:24 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Person from './Person/Person'
+
 class App extends Component {
+  state = {
+    persons: [
+      { id: '1', name: 'Rust', age: '30' },
+      { id: '2', name: 'Howard', age: '29' },
+      { id: '3', name: 'Danny', age: '28' }
+    ],
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  deletePersonHandler = (personIndex) => {
+      const persons = [...this.state.persons];
+      persons.splice(personIndex,1);
+      this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+  }
+
   render() {
+
+    const style = {
+      backgroundColor : 'white',
+      font: 'inherit',
+      border: '1px solid grey',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person,index) => {
+            return <Person 
+                      click = {() => this.deletePersonHandler(index)}
+                      name={person.name}
+                      age={person.age}
+                      key={person.id}
+                      changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+          
+        </div>
+      )
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <h1>Hey React, Ssup</h1>
+        <p>This is working</p>
+
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+
+        { persons }    
+        
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
